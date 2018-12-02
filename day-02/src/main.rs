bed30b7aa1921b43894103cb758f9b142bcb6fc4
Bryan Burgers <bryan@burgers.io>
2018-12-02 14:36:04 -0600

    Day 02b

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -21,6 +21,23 @@ fn main() {
         .count();
 
     println!("{} * {} = {}", twos, threes, twos * threes);
+
+    let mut best = 0;
+    for i in input.clone().iter() {
+        for j in input.clone().iter() {
+            if i == j {
+                continue;
+            }
+
+            let res = compare(i, j);
+            let val = res.len();
+            if val > best {
+                best = val;
+
+                println!("{}, {} => {} ({})", i, j, val, res);
+            }
+        }
+    }
 }
 
 fn has_exactly(v: &str, n: usize) -> bool {
@@ -34,4 +51,15 @@ fn has_exactly(v: &str, n: usize) -> bool {
     });
 
     r.values().any(|x| *x == n)
+}
+
+fn compare(v1: &str, v2: &str) -> String {
+    let i1 = v1.chars();
+    let i2 = v2.chars();
+    let matches: String = i1.zip(i2)
+        .filter(|(a, b)| a == b)
+        .map(|(a, _)| a)
+        .collect();
+
+    matches
 }
